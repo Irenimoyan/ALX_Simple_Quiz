b529b8425b588d9b4b4b0d459307149bf1f69320
Irenimoyan <sheutomalli@gmail.com>
2025-06-13 18:57:32 +0100

    created an quiz app

---
+++ quiz.js
@@ -0,0 +1,26 @@
+// Define the CheckAnswer function
+function checkAnswer() {
+    // Declare the Correct Answer
+    const correctAnswer = "4";
+
+    // Get the user's answer
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+
+    // if no option is selected, display an error message
+    if (!selectedOption) {
+        document.getElementById('feedback').textContent = "Please select an answer before submit.";
+        return;
+    }
+
+    const userAnswer = selectedOption.value;
+
+    // compare and give feedback
+
+    if (userAnswer === correctAnswer) {
+        document.getElementById('feedback').textContent = "Correct! Well done.";    
+    } else {
+        document.getElementById('feedback').textContent="Incorrect answer! Try Again"
+    }
+}
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
